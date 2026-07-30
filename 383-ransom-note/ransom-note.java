7b51dc74db24f86cf8787c5e38da40caaa766512
sakshiverma22 <154626693+sakshiverma22@users.noreply.github.com>
2026-07-30 23:16:55 +0530

    Time: 16 ms (18.61%) | Memory: 47.1 MB (20.03%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,14 +1,27 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] freq=new int[26];
-        //count letters in magazine
-        for(char ch:magazine.toCharArray()){
-            freq[ch-'a']++;
+        // int[] freq=new int[26];
+        // //count letters in magazine
+        // for(char ch:magazine.toCharArray()){
+        //     freq[ch-'a']++;
+        // }
+        // //use letters for ransomNote
+        // for(char ch:ransomNote.toCharArray()){
+        //     freq[ch-'a']--;
+        //     if(freq[ch-'a']<0) return false;
+        // }
+        // return true;
+        HashMap<Character, Integer> map=new HashMap<>();
+        //count fre of chars in magazine
+        for(char ch: magazine.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
-        //use letters for ransomNote
+        //try to construct ransomNote
         for(char ch:ransomNote.toCharArray()){
-            freq[ch-'a']--;
-            if(freq[ch-'a']<0) return false;
+            if(!map.containsKey(ch) || map.get(ch)==0){
+                return false;
+            }
+            map.put(ch,map.get(ch)-1);
         }
         return true;
     }
