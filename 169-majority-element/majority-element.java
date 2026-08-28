4ea5bb4768a3c7db96d154b40fc1e53728c88bb5
sakshiverma22 <154626693+sakshiverma22@users.noreply.github.com>
2026-08-28 21:11:40 +0530

    Time: 2 ms (70.91%) | Memory: 55.8 MB (17.96%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,34 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-        int cnt = 0;
-        int el = 0;
-
-        // Moore's Voting Algorithm
-        for(int i = 0; i < nums.length; i++) {
-            if(cnt == 0) {
-                el = nums[i];
-                cnt = 1;
-            }
-            else if(nums[i] == el) {
-                cnt++;
-            }
-            else {
-                cnt--;
-            }
-        }
-
-        // Verification (optional for LeetCode 169)
-        int cnt1 = 0;
-
-        for(int num : nums) {
-            if(num == el)
-                cnt1++;
-        }
-
-        if(cnt1 > nums.length / 2)
-            return el;
-
-        return -1;
+        int n=nums.length;
+        int cnt=0, el=0;
+        for(int i=0;i<n;i++){
+                if(cnt==0){
+                    el=nums[i];
+                    cnt=1;
+                }else if(el==nums[i]){
+                    cnt++;
+                }else{
+                    cnt--;
+                }
+        } 
+        return el;
     }
 }
